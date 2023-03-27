4c35d2513a74284859662bac61895faf096f743e
raabele1227 <rachel_abele@yahoo.com>
2023-03-26 21:47:05 -0400

    Refactor the app for updating an existing story

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -49,27 +49,54 @@ exports.show = (req, res, next) => {
 
 exports.edit = (req, res, next) => {
   let id = req.params.id;
-  let story = model.findById(id);
-  if (story) {
-    res.render("./story/edit", { story });
-  } else {
-    let err = new Error("Cannot find a story with id " + id);
-    err.status = 404;
-    next(err);
+  // an ObjectId is a 24-bit hex string
+  if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+    let err = new Error("Invalid story id");
+    err.status = 400;
+    return next(err);
   }
+  model
+    .findById(id)
+    .then((story) => {
+      if (story) {
+        return res.render("./story/edit", { story });
+      } else {
+        let err = new Error("Cannot find a story with id " + id);
+        err.status = 404;
+        next(err);
+      }
+    })
+    .catch((err) => next(err));
 };
 
 exports.update = (req, res, next) => {
   let story = req.body;
   let id = req.params.id;
 
-  if (model.updateById(id, story)) {
-    res.redirect("/stories/" + id);
-  } else {
-    let err = new Error("Cannot find a story with id " + id);
-    err.status = 404;
-    next(err);
+  if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+    let err = new Error("Invalid story id");
+    err.status = 400;
+    return next(err);
   }
+
+  model
+    .findByIdAndUpdate(id, story, {
+      useFindAndModify: false,
+      runValidators: true,
+    })
+    .then((story) => {
+      if (story) {
+        res.redirect("/stories/" + id);
+      } else {
+        let err = new Error("Cannot find a story with id " + id);
+        err.status = 404;
+        next(err);
+      }
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") err.status = 400;
+      next(err);
+    });
 };
 
 exports.delete = (req, res, next) => {
